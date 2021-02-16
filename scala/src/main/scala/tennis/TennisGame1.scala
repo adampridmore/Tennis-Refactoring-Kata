3212d31b6e1a66371816836dc8f2860fdb221ca4
adampridmore <adam_pridmore@hotmail.com>
2021-02-16 21:43:43 +0000

    Rfactored TennisGame

--- scala/src/main/scala/tennis/TennisGame1.scala
+++ scala/src/main/scala/tennis/TennisGame1.scala
@@ -5,50 +5,52 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
   var m_score1: Int = 0
   var m_score2: Int = 0
 
-  def wonPoint(playerName : String) {
-          if (playerName == "player1")
-              m_score1 += 1
-          else
-              m_score2 += 1
-      }
-
-  def calculateScore() : String = {
-      var score : String = ""
-      var tempScore=0
-      if (m_score1==m_score2)
-      {
-        score = m_score1 match {
-              case 0 =>  "Love-All"
-              case 1 => "Fifteen-All"
-              case 2 => "Thirty-All"
-              case _ => "Deuce"
-
-          }
-      }
-      else if (m_score1>=4 || m_score2>=4)
-      {
-          val minusResult = m_score1-m_score2
-          if (minusResult==1) score ="Advantage player1"
-          else if (minusResult == -1) score ="Advantage player2"
-          else if (minusResult>=2) score = "Win for player1"
-          else score ="Win for player2"
-      }
-      else
-      {
-          for ( i<- 1 until 3 by 1)
-          {
-              if (i==1) tempScore = m_score1
-              else { score+="-"; tempScore = m_score2;}
-              val tempScore2 = tempScore match {
-                  case 0 => "Love"
-                  case 1 => "Fifteen"
-                  case 2 => "Thirty"
-                  case 3 => "Forty"
-              }
-            score += tempScore2
-          }
-      }
-    return score
+  def wonPoint(playerName : String) : Unit = {
+    if (playerName == "player1")
+      m_score1 += 1
+    else
+      m_score2 += 1
   }
 
+  def calculateScore() : String = {
+    DisplayScore.generate(m_score1, m_score2)
+  }
+}
+
+object DisplayScore {
+  def generate(score1: Int, score2: Int) : String = {
+
+    val difference = score1 - score2
+    val tieBreak = score1 >= 4 || score2 >= 4
+    
+    (score1, score2, difference, tieBreak) match {
+      case (_,_, 0, _) if score1 == score2 => drawing(score1)
+      case (score1, score2, _, false) => basicScore(score1, score2)
+      case (score1, score2, 1, true) => "Advantage player1"
+      case (score1, score2, -1, true) => "Advantage player2"
+      case (_ , _, difference, true) if difference > 0  => "Win for player1"
+      case (_ , _, difference, true) if difference < 0 => "Win for player2"
+    }
+  }
+
+  private def drawing(score: Int) : String = {
+    score match {
+      case 0 | 1 | 2 => s"${scoreName(score)}-All"
+      case _ => "Deuce"
+    }
+  }
+
+  private def basicScore(score1: Int, score2: Int) : String = {
+    s"${scoreName(score1)}-${scoreName(score2)}"
+  }
+
+  private def scoreName(score: Int) = {
+    score match {
+      case 0 => "Love"
+      case 1 => "Fifteen"
+      case 2 => "Thirty"
+      case 3 => "Forty"
+      case unknown => throw new RuntimeException(s"Unknown score: $unknown")
+    }
+  }
 }
